a171ec2ab8d4388b73a071f53bcc99a67fb15573
Evgeny Smagin <snailman7297@gmail.com>
2021-12-25 14:23:45 +0500

    added guards

--- src/router/index.js
+++ src/router/index.js
@@ -3,26 +3,55 @@ import UserPage from "../pages/UserPage";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
 import ForgotPasswordPage from "../pages/ForgotPasswordPage";
+import isAuthorized from "../custom/auth";
 const routes = [
   {
     path: '/',
     name: 'UserPage',
     component: UserPage,
+    beforeEnter: (to, from, next) => {
+      if (!isAuthorized()) {
+        next('/login');
+      } else {
+        next();
+      }
+    }
   },
   {
     path: '/login',
     name: 'LoginPage',
     component: LoginPage,
+    beforeEnter: (to, from, next) => {
+      if (isAuthorized()) {
+        next('/');
+      } else {
+        next();
+      }
+    }
   },
   {
     path: '/register',
     name: 'RegisterPage',
     component: RegisterPage,
+    beforeEnter: (to, from, next) => {
+      if (isAuthorized()) {
+        next('/');
+      } else {
+        next();
+      }
+    }
   },
   {
     path: '/forgot-password',
     name: 'ForgotPasswordPage',
     component: ForgotPasswordPage,
+    beforeEnter: (to, from, next) => {
+      if (isAuthorized()) {
+        next('/');
+      } else {
+        next();
+      }
+    }
   },
 ]
 
