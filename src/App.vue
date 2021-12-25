86bebdaeaa4c9519833f7e536968828e26f28006
Evgeny Smagin <snailman7297@gmail.com>
2021-12-25 14:08:40 +0500

    added global styles to app component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+$main-color: #1f1f1f;
+$secondary-color: #fcc23c;
+$danger-color: #ff5722;
+$light-color: #fff;
+$font: 'Roboto', sans-serif;
+
+* {
+  margin: 0;
+  box-sizing: border-box;
+  font-family: $font;
 }
 
-#nav {
-  padding: 30px;
+h1,h2,h3,h4,h5 {
+  color: $main-color;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+body {
+  height: 100vh;
+  background-color: $main-color;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#app {
+  height: 100%;
 }
 </style>
