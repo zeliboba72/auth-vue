4fd33b77f4eca450e8db1450b7ee2f321879f29d
Evgeny Smagin <snailman7297@gmail.com>
2021-12-25 15:02:44 +0500

    added global styles

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,6 @@
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
-$main-color: #1f1f1f;
-$secondary-color: #fcc23c;
-$danger-color: #ff5722;
-$light-color: #fff;
-$font: 'Roboto', sans-serif;
-
-* {
-  margin: 0;
-  box-sizing: border-box;
-  font-family: $font;
-}
 
 h1,h2,h3,h4,h5 {
   color: $main-color;
