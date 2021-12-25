fda713dd9299b5c046ffd740779439dee159d7f9
Evgeny Smagin <snailman7297@gmail.com>
2021-12-25 14:17:26 +0500

    created component app-form

---
+++ src/components/AppForm.vue
@@ -0,0 +1,61 @@
+<template>
+  <form class="form" @submit.prevent="$emit('submit')">
+    <h1 class="title">{{ title }}</h1>
+    <p class="error" v-if="hasError">{{ errorText }}</p>
+    <slot/>
+    <app-button :text="submitText" type="submit"/>
+    <div class="footer">
+      <slot name="footer"/>
+    </div>
+  </form>
+</template>
+
+<script>
+import AppButton from "./AppButton";
+export default {
+  name: 'AppForm',
+  components: {
+    AppButton,
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    submitText: {
+      type: String,
+      required: true,
+    },
+    errorText: {
+      type: String,
+    },
+    hasError: {
+      type: Boolean,
+    },
+  },
+  emits: ['submit'],
+}
+</script>
+
+<style lang="scss" scoped>
+.form {
+  width: 100%;
+  max-width: 500px;
+  padding: 30px;
+  border: 4px solid $secondary-color;
+  background-color: $light-color;
+}
+
+.title {
+  margin-bottom: 20px;
+}
+
+.error {
+  color: $danger-color;
+  margin-bottom: 20px;
+}
+
+.footer {
+  margin-top: 20px;
+}
+</style>
