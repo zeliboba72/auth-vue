d50678e7f931c95ea3274cac981988843100381d
Evgeny Smagin <snailman7297@gmail.com>
2021-12-25 14:11:27 +0500

    created component app-wrapper

---
+++ src/components/AppWrapper.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app-wrapper">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppWrapper"
+}
+</script>
+
+<style lang="scss" scoped>
+.app-wrapper {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
