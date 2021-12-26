79d00320a3f422203c001be35918f1f7348898f5
Evgeny Smagin <snailman7297@gmail.com>
2021-12-26 16:14:46 +0500

    changed main.js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import axios from "axios";
 
-const app = createApp(App);
-app.use(store);
-app.use(router);
-app.config.globalProperties.axios = axios;
 createApp(App).use(store).use(router).mount('#app')
